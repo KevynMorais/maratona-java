f168ee7069122ca3092599735f8f427b576183d7
Kevyn Morais <kevynmorais721@gmail.com>
2026-07-07 16:53:42 -0300

    Casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,18 +3,19 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
-            
+
         System.out.println("A idade é "+idade+" anos");
         System.out.println(falso);
         System.out.println("char "+caractere);
+        System.out.println(numeroGrande);
     }
 }
