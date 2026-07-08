b7aff01099f852214d1d33aef6a76ffa628d2911
Kevyn Morais <kevynmorais721@gmail.com>
2026-07-08 18:05:17 -0300

    Operadores Lógicos

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -23,5 +23,21 @@ public class Aula04Operadores {
         System.out.println("isDezIgualDez: "+isDezIgualDez);
         System.out.println("isDezIgualVinte: "+isDezIgualVinte);
         System.out.println("isDezDiferenteDez: "+isDezDiferenteDez);
+
+        // && (AND) || (OR) !
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMenorQueTrinta: "+isDentroDaLeiMenorQueTrinta);
+        System.out.println("isDentroDaLeiMaiorQueTrinta: "+isDentroDaLeiMaiorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel: "+isPlaystationCincoCompravel);
+        
     }
 }
