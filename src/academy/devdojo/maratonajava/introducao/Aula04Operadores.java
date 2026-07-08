b553eab5d464bd04786c3a199d6717cf0ce88324
Kevyn Morais <kevynmorais721@gmail.com>
2026-07-07 22:39:01 -0300

    Operadores Aritméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
